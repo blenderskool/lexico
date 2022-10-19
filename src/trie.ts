af13311ff3131e2f0b9c06fd59846a5de9c23dc0
Akash Hamirwasia <akash.hamirwasia@razorpay.com>
2022-10-20 00:22:54 +0530

    Add `PrefixCmp` - comparators which performs prefix search
    This comparator performs a prefix search over all the records
    unlike `BinaryCmp` which does not restrict the parts of string
    matched.
    
    It is a search indexed comparator and uses tries to perform
    extremely fast searches over indexed fields ⚡️

---
+++ src/trie.ts
@@ -0,0 +1,53 @@
+class Node<T> {
+  nodes: Record<string, Node<T>> = {};
+  end: T[];
+}
+
+/**
+ * Prefix based trie that stores the record(referred to as `value`)
+ * in the array of "end" nodes.
+ */
+export class Trie<T> {
+  trie: Node<T> = new Node();
+
+  add(key: string, value: T) {
+    key = key.toLowerCase();
+
+    let top = this.trie;
+    for (let i = 0; i < key.length; ++i) {
+      const ch = key[i];
+
+      top.nodes[ch] ??= new Node<T>();
+      top = top.nodes[ch];
+    }
+
+    top.end ??= [];
+    top.end.push(value);
+  }
+
+  searchAll(key: string): T[] {
+    key = key.toLowerCase();
+
+    let top = this.trie;
+    for (let i = 0; i < key.length; ++i) {
+      const ch = key[i];
+      if (!top.nodes[ch]) return [];
+
+      top = top.nodes[ch];
+    }
+
+    // DFS through all children nodes as they are part of the result
+    return this.getAllValues(top);
+  }
+
+  private getAllValues(top: Node<T>): T[] {
+    if (top.end) return top.end;
+
+    const result: T[] = [];
+    Object.values(top).forEach((node) => {
+      result.concat(this.getAllValues(node));
+    });
+
+    return result;
+  }
+}
